09eb10c297c010acc55a71bcecc9107acfb8a7b3
Ragnhild Elise Dirdal <ragnhilddirdal@eduroam-193-157-225-109.wlan.uio.no>
2020-10-01 16:30:12 +0200

    Ferdig med 2c. Prover aa implementere 2d.

--- oppgave2.py
+++ oppgave2.py
@@ -2,64 +2,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def dydt(y, t):
-    return f
+def f(t, y):
+    return np.cos(t)
 
 
-def runge_kutta_method(t0, y0, t, n):
-    dt = (max(t) - t0) / float(n)
-    # dt = 0.01
-    n = int(n)
-    y = y0
-
-    for i in range(
-        1, int(n + 1)
-    ):  # her skal det egentlig stå n + 1 men fikk feilmelding.
-        rk_1 = dt * dydt(t0, y)
-        rk_2 = dt * dydt((t0 + (dt / 2)), (y + (rk_1 / 2)))
-        rk_3 = dt * dydt((t0 + (dt / 2)), (y + (rk_2 / 2)))
-        rk_4 = dt * dydt((t0 + (dt / 2)), (y + rk_3))
-
-        y = y + (1 / 6) * (rk_1 + 2 * rk_2 + 2 * rk_3 + rk_4)
-        t0 = t0 + dt
+def runge_kutta_method(tn, yn, dt, f):
+    rk_1 = dt * f(tn, yn)
+    rk_2 = dt * f((tn + (dt / 2)), (yn + (rk_1 / 2)))
+    rk_3 = dt * f((tn + (dt / 2)), (yn + (rk_2 / 2)))
+    rk_4 = dt * f((tn + (dt / 2)), (yn + rk_3))
+    y = yn + (1 / 6) * (rk_1 + 2 * rk_2 + 2 * rk_3 + rk_4)
     return y
 
 
 """
-def test_runge_kutta_method():
-    expected = np.cos(2 * np.pi)
-    # f = np.cos(2 * np.pi)
-    dydt(np.cos(2 * np.pi), 2 * np.pi)
-    computed = runge_kutta_method(0, 0, 2 * np.pi, 100)
-    # print(computed)
-    # print(expected)
-    # plt.plot(1, expected)
-    # plt.plot(1, computed)
-    # plt.show()
-    tol = 1e-9
-    # assert abs(computed - expected) < tol
-"""
+n = 1000
+yn = np.zeros(n)
+tn = np.linspace(0, 2 * np.pi, n)
+dt = (tn[-1] - tn[0]) / n
 
-t = np.linspace(0, 2 * np.pi, 50)
-f = np.cos(t)
-y = runge_kutta_method(0, 0, t, 50)
-# print(np.cos(2 * np.pi))
-print(y)  # printer 0.5403
-# print(f)
-# test_runge_kutta_method()
-# plt.plot(t, f, label="expected", linewidth=4.3)
-# plt.plot(t, o, label="computed")
-# plt.legend()
-# plt.show()
+for i in range(n - 1):
+    yn[i + 1] = runge_kutta_method(tn[i], yn[i], dt, f)
+
+plt.title("f(t,y) = cos(t)")
+plt.plot(tn, np.sin(tn), label="expected", linewidth=4.3)
+plt.plot(tn, yn, label="computed")
+plt.legend()
+plt.savefig("Oppgave 2c")
+plt.show()
+
+"""
 
 
 def dcndt(c, t):
-    dt = 0.05
-    who = 1.4
-    w = 1.0
+
     h_ = 1
+    q = -1
     me = 1
     e_ = 1
     En = h_ * w * (n + 0.5)
     Em = h_ * w * (m + 0.5)
     vdt = Vnm(t) * np.exp((i * (En - Em) * t) / h_)
+
+    x_0 = np.sqrt(h_ / (me * w))
+
+    diag = np.sqrt(np.arange(1, n))
+    V0 = np.zeros([n, n], dtype=complex)
+    V0 += np.diag(diag, 1) + np.diag(diag, -1)
+    V0 = V0 * (x0 / (np.sqrt(2)))
+
+    def V_dt(t):
+        V = V0 * (-q) * e_0 * np.cos(w * t) * np.exp((1j(En - Em) * t) / h_)
+
+    init = np.zeros(n)
+
+
+# 1j = "i" i python
+"""
+dt = 0.01
+who = 1.4
+w = 1.0
+e0_1 = 0.05
+e0_2 = 0.2
+e0_3 = 0.5
+"""
