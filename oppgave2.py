79e57a2f67a67a82a27524d7f12fd21e91598c1e
Ragnhild Elise Dirdal <ragnhilddirdal@Ragnhilds-MBP.home>
2020-10-06 21:29:39 +0200

    Har endret En og Em med andre uttrykk. Usikker paa hva absorption probability er enda.

--- oppgave2.py
+++ oppgave2.py
@@ -31,7 +31,6 @@ def runge_kutta_method(tn, yn, dt, f):
 # plt.show()
 
 
-# dt = 0.01
 who = 1.4
 w = 1.0
 h_ = 1
@@ -40,7 +39,7 @@ me = 1
 e_ = 1
 
 e0_1 = 0.05
-e0_2 = 0.2
+e0_1 = 0.2
 e0_3 = 0.5
 n_states = 3
 
@@ -52,14 +51,6 @@ T = 100
 
 x_0 = np.sqrt(h_ / (me * w))
 
-
-# init = np.zeros(n)
-
-
-# må bytte ut cos(t) med -i/h_V't*C
-
-# t = 0
-# n_timestep = 0.01
 c = np.zeros([num, n_states], dtype=complex)
 c[0, 0] = 1
 # c1 = c * np.exp((1j * En * t) / h_)
@@ -68,6 +59,7 @@ c[0, 0] = 1
 
 # 1j = "i" i python
 tn = np.linspace(0, T, num)
+# dt = tn[1] - tn[0]
 dt = 0.01
 
 
@@ -75,9 +67,14 @@ def f1(t, c):
     diag = np.sqrt(np.arange(1, n_states))
     V0 = np.zeros([n_states, n_states], dtype=complex)
     V0 += np.diag(diag, 1) + np.diag(diag, -1)
-    V0 *= x_0 / (np.sqrt(2))
-    V = V0 * (-q) * e0_1 * np.cos(w * t) * np.exp((1j * (En - Em) * t) / h_)
-    return (-1j / h_) * np.dot(V, c)
+    # V0 *=
+    V = V0 * (
+        (x_0 / (np.sqrt(2))) * (-q) * e0_1 * np.cos(w * t) * np.exp((1j * who * t))
+    )  # her mangler En osv
+    # print(V)
+    return (-1j / h_) * np.dot(
+        V, c
+    )  # * np.exp((1j * e0_1 * np.cos(w * t) * t) / h_)  # byttet ut En med e_0*cos(wt)
 
 
 for i in range(num - 1):
@@ -85,18 +82,13 @@ for i in range(num - 1):
 
 
 def absorption_probability(t):
-    pass
+    return ((e0_1 * np.sqrt(1 / 2 * who))) ** 2 * np.sin((1 / 2) * (who - w) * t) * np
 
 
 c__ = np.abs(c) ** 2
 
-# plt.plot(tn, c__[:, 0], label="computed1")
-# plt.plot(tn, c__[:, 1], label="computed2")
+plt.plot(tn, c__[:, 0], label="computed1")
+plt.plot(tn, c__[:, 1], label="computed2")
 plt.plot(tn, c__[:, 2], label="computed3")
 plt.legend()
 plt.show()
-
-# dt = tn[1] - tn[0]
-# c1 = np.zeros([100, n])
-# c1[0, 0] = 1
-# print(c1)
