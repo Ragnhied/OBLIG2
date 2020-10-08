22e52cb5a3352deaf6c243997523fb6e012b4ee9
Ragnhild Elise Dirdal <ragnhilddirdal@Ragnhilds-MBP.home>
2020-10-08 16:07:08 +0200

    Plottet figurene og lagret dem

--- oppgave2.py
+++ oppgave2.py
@@ -39,12 +39,12 @@ me = 1
 e_ = 1
 
 e0_1 = 0.05
-e0_1 = 0.2
+e0_2 = 0.2
 e0_3 = 0.5
 n_states = 3
 
-En = h_ * w * (n_states + 0.5)
-Em = h_ * w * (n_states + 0.5)
+En = h_ * who * (n_states + 0.5)
+
 
 num = 10000
 T = 100
@@ -74,7 +74,7 @@ def f1(t, c):
     # print(V)
     return (-1j / h_) * np.dot(
         V, c
-    )  # * np.exp((1j * e0_1 * np.cos(w * t) * t) / h_)  # byttet ut En med e_0*cos(wt)
+    )  # * np.exp((1j * En * t) / h_)  # byttet ut En med e_0*cos(wt)
 
 
 for i in range(num - 1):
@@ -82,13 +82,23 @@ for i in range(num - 1):
 
 
 def absorption_probability(t):
-    return ((e0_1 * np.sqrt(1 / 2 * who))) ** 2 * np.sin((1 / 2) * (who - w) * t) * np
+    diag = np.sqrt(np.arange(1, n_states))
+    V0 = np.zeros([n_states, n_states], dtype=complex)
+    V0 += np.diag(diag, 1) + np.diag(diag, -1)
+    W_fi_2 = (V0 * (x_0 / np.sqrt(2)) * ((-q * e0_1) / 2)) ** 2
+    return (W_fi_2 * np.sin(0.5 * (who - w) * t) ** 2) / (
+        h_ ** 2 * (0.5 * (who - w)) ** 2
+    )
 
 
 c__ = np.abs(c) ** 2
 
-plt.plot(tn, c__[:, 0], label="computed1")
-plt.plot(tn, c__[:, 1], label="computed2")
-plt.plot(tn, c__[:, 2], label="computed3")
+
+# plt.plot(tn, absorption_probability(tn), label="Absorption probability")
+plt.plot(tn, c__[:, 0], label="c0^2")
+plt.plot(tn, c__[:, 1], label="c1^2")
+# plt.plot(tn, c__[:, 2], label="computed3")
 plt.legend()
+plt.title("c0^2 and c1^2 with e0 = 0.5")
+# plt.savefig("2e e0=0,5.png")
 plt.show()
